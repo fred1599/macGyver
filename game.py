9b3aeadcb22b00ad815c4e60354783d42dfd02af
fred1599 <fred1599@hotmail.com>
2018-11-30 19:53:10 +0100

    Update game.py
    various improvements

--- game.py
+++ game.py
@@ -16,11 +16,12 @@ pygame.init()
 pygame.font.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-# list of surface in game
+# list of surfaces in game
 OBJECTS = {}
 for f in glob.iglob(PATH_CURRENT_FILE + '/**/*.png', recursive=True):
     filename, ext = os.path.splitext(os.path.basename(f))
     OBJECTS[filename] = pygame.image.load(f).convert()
+
 
 def load_level(n):
     """
@@ -35,6 +36,7 @@ def load_level(n):
             grounds.append(objects)
     return grounds
 
+
 def add_warn_object(surface, grounds, obj, speed):
     while True:
         line_rand = randint(0, len(grounds)-1)
@@ -45,12 +47,14 @@ def add_warn_object(surface, grounds, obj, speed):
             surface.blit(obj, (column_rand*speed, line_rand*speed))
             break
 
+
 def draw(grounds, pixels):
     obj = ("floor23", "floor15", "MacGyver", "guardian")
     for j, ground in enumerate(grounds):
         for i, n in enumerate(ground):
             if n >= 0:
                 screen.blit(OBJECTS[obj[n]], (i*20, j*20))
+
 
 def move(player, grounds, direction):
     directions = [(1, 0), (-1, 0), (0, -1), (0, 1)]
@@ -59,19 +63,16 @@ def move(player, grounds, direction):
     for i, line in enumerate(grounds):
         if player in line:
             index = line.index(player)
-            new_index = index + x
-            new_line = i + y
+            new_index = index + x # new column of player
+            new_line = i + y # new line of player
             if 0 <= new_index < column_max:
-                try:
-                    if grounds[new_line][new_index] == -1:
-                        return False
-                    elif grounds[new_line][new_index] == 3:
-                        return True
-                    elif grounds[new_line][new_index] != 0:
-                        grounds[new_line][new_index] = player
-                        grounds[i][index] = 1 # floor
-                except IndexError:
-                    pass # do nothing
+                if grounds[new_line][new_index] == -1: # collision dangerous objects
+                    return False # Quit Game with message "you lose"
+                elif grounds[new_line][new_index] == 3: # collision guardian
+                    return True # Quit Game with message "you win"
+                elif grounds[new_line][new_index] != 0: # collision floor
+                    grounds[new_line][new_index] = player
+                    grounds[i][index] = 1 # floor
                 break
 
 
@@ -81,6 +82,7 @@ def draw_result(win=True):
     surf = pygame.font.SysFont('helvetica', 18).\
             render(text, True, (255, 255, 255))
     screen.blit(surf, (50, 50))
+
 
 class Game:
     RIGHT, LEFT = 0, 1
@@ -92,9 +94,8 @@ class Game:
 
         self.grounds = load_level(start)
         draw(self.grounds, Game.SPEED)
-        add_warn_object(screen, self.grounds, OBJECTS["aiguille"], Game.SPEED)
-        add_warn_object(screen, self.grounds, OBJECTS["ether"], Game.SPEED)
-        add_warn_object(screen, self.grounds, OBJECTS["seringue"], Game.SPEED)
+        for name in ("aiguille", "ether", "seringue"):
+            add_warn_object(screen, self.grounds, OBJECTS[name], Game.SPEED)
 
     def quit(self):
         """exit game"""
@@ -110,20 +111,14 @@ class Game:
             self.clock.tick(60)
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        res = move(Game.PLAYER, self.grounds, Game.RIGHT)
-                    elif event.key == pygame.K_LEFT:
-                        res = move(Game.PLAYER, self.grounds, Game.LEFT)
-                    elif event.key == pygame.K_UP:
-                        res = move(Game.PLAYER, self.grounds, Game.UP)
-                    elif event.key == pygame.K_DOWN:
-                        res = move(Game.PLAYER, self.grounds, Game.DOWN)
-
-                    draw(self.grounds, Game.SPEED)
-                    if res == False or res == True:
-                        draw_result(res)
-                        self.continu = False
-                    pygame.display.flip()
+                    for attr in ('RIGHT', 'LEFT', 'UP', 'DOWN'):
+                        if event.key == getattr(pygame, f'K_{attr}'):
+                            res = move(Game.PLAYER, self.grounds, getattr(Game, attr))
+                            draw(self.grounds, Game.SPEED)
+                            if res != None:
+                                draw_result(res)
+                                self.continu = False
+                        pygame.display.flip()
             pygame.display.flip()
 
 game = Game()
